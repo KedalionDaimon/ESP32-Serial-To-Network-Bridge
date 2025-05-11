592386a4729b7aeb6b18e892555eceae27478ca8
Nino Ivanov <kedalion.daimon@gmail.com>
2025-05-11 10:39:29 +0200

    increase serial buffer size

--- BrotherEP22HardcopyTerminalWithMargin.ino
+++ BrotherEP22HardcopyTerminalWithMargin.ino
@@ -1,3 +1,12 @@
+// LOLIN(WEMOS) D1 R32 & mini (ESP8266)
+// ESP32 Wrover - for example
+// telnet 192.168.4.1 23
+// or
+// nc -C 192.168.4.1 23
+#pragma GCC optimize ("-O3")
+#pragma GCC push_options
+#define SERIAL_BUFFER_SIZE 2048
+
 #define SRL Serial
 #include <WiFi.h>
 const char* ssid = "BrotherEP22";
@@ -8,6 +17,7 @@ unsigned char ch;
 void setup() {
   int maxcounter = 0;
   SRL.begin(300, SERIAL_8N1);
+  Serial.setRxBufferSize(SERIAL_BUFFER_SIZE);
   WiFi.softAP(ssid, password); delay(500);
   SRL.print("    WiFi");
   wifiServer.begin();
